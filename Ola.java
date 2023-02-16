b090f2f98c0625337b11c2bb9e1f61a9de94ca97
Victor Amaral <victortavamaral@gmail.com>
2023-02-15 22:00:42 -0300

    Add files via upload

---
+++ Ola.java
@@ -0,0 +1,14 @@
+public class Ola {
+    public static void main(String[] args) {
+        // System of a Dawn
+        System.out.println("System of a Dawn");
+        System.out.print("eh us guri");
+
+        // Pra rodar
+        // > javac Nomedoarquivo.java       chama o compilador do java pra transformar o código em executável
+        // > java Nomedoarquivo             roda o código
+
+        // println para printar e quebrar a linha
+        // printf  para printar formatado
+    }
+}
